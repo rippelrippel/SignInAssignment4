6158f7ef4578da6d2ab1e5bab8506e949556a4d6
Noa Rippel <rippelnoa@gmail.com>
2026-03-11 16:06:34 +0200

    craete SignInViewModel empty cunstructor

---
+++ SignInAssignment4/ViewModels/SignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignInAssignment4.ViewModels
+{
+    public partial class SignInViewModel:ObservableObject
+    {
+        public SignInViewModel()
+        {
+        }
+    }
+}
